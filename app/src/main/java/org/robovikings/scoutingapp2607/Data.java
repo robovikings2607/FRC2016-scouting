f90d837fc4e9adec12194442482a6990d93311d3
Tom Harris <t.harris.dot@gmail.com>
2016-02-13 12:27:23 -0500

    testing commit to GitHub from Android Studio

---
+++ app/src/main/java/org/robovikings/scoutingapp2607/Data.java
@@ -0,0 +1,13 @@
+package org.robovikings.scoutingapp2607;
+
+/**
+ * Created by Tom on 2/10/2016.
+ */
+public class Data {
+    static int[] defenses = new int[10];
+
+    static int blhGoal, blLGoal, blMiss, rdhGoal, rdlGoal, rdMiss, fouls, techFouls;
+
+    boolean challenge, scale, spy, broken, absent, reach;
+
+}
